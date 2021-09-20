51ab5b8c0bf710b858fe2e1184412f3900260cc9
crssnky <vindicare28@gmail.com>
2021-09-20 23:58:09 +0900

    first release

---
+++ src/announce.rs
@@ -0,0 +1,8 @@
+// use serenity::model::channel::Message;
+// use serenity::model::id::ChannelId;
+// use serenity::prelude::Context;
+// use serenity::Result;
+
+// pub async fn announce(ctx: &Context, cid: &ChannelId, mes: &str) -> Result<Message> {
+//   cid.send_message(&ctx, |m| m.content(mes)).await
+// }
