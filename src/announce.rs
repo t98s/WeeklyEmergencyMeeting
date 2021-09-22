5762a9f6cdc0cc04e0b93a3d5d9470ba50d826e7
crssnky <vindicare28@yahoo.co.jp>
2021-09-23 02:11:05 +0900

    remove unuse files

--- src/announce.rs
+++
@@ -1,8 +0,0 @@
-// use serenity::model::channel::Message;
-// use serenity::model::id::ChannelId;
-// use serenity::prelude::Context;
-// use serenity::Result;
-
-// pub async fn announce(ctx: &Context, cid: &ChannelId, mes: &str) -> Result<Message> {
-//   cid.send_message(&ctx, |m| m.content(mes)).await
-// }
